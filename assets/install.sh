4fa0edc4b6c23277e4ac87dde6643ffb51dfb1a6
Wiktor Żurawik <w.zurawik@nullthepointer.com>
2017-07-21 00:53:40 +0200

    Add support for .pem certificates
    Support of those files comes in handy while using, e.g. LetsEncrypt
    certificates in TLS.

--- assets/install.sh
+++ assets/install.sh
@@ -59,6 +59,26 @@ if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/ce
   postconf -e smtpd_tls_cert_file=$(find /etc/postfix/certs -iname *.crt)
   postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname *.key)
   chmod 400 /etc/postfix/certs/*.*
+
+  # /etc/postfix/master.cf
+  postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
+  postconf -P "submission/inet/syslog_name=postfix/submission"
+  postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
+  postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
+  postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
+  postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
+elif [[ -n "$(find /etc/postfix/certs -iname *.pem)" ]]; then
+  # /etc/postfix/main.cf
+  postconf -e smtpd_tls_cert_file=$(find /etc/postfix/certs -iname fullchain.pem)
+  postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname privkey.pem)
+  postconf -e smtpd_tls_loglevel=1
+  postconf -e smtp_use_tls=yes
+  postconf -e smtp_tls_protocols=\!SSLv2,\!SSLv3
+  postconf -e smtpd_tls_protocols=\!SSLv2,\!SSLv3
+  postconf -e smtpd_tls_ciphers=high
+  postconf -e smtp_tls_ciphers=high
+  chmod 400 /etc/postfix/certs/*.*
+
   # /etc/postfix/master.cf
   postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
   postconf -P "submission/inet/syslog_name=postfix/submission"
